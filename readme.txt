8c3a853bf2820461455a868cba2a112f6c7d5e9a
litonarefin <litonice09@gmail.com>
2023-10-20 00:10:31 +0600

    initial commit

---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Tweakly ===
+Contributors: litonice13, jwthemeltd
+Donate link: https://www.buymeacoffee.com/jeweltheme
+Tags: tweaks, fixings, enhancement, admin tweaks
+Requires at least: 4.0
+Tested up to: 6.3.2
+Stable tag: 1.0.0
+Requires PHP: 5.6
+License: GPLv3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Admin Tweaks Plugin
+
+== Description ==
+WordPress Admin Customization
+
+== Features ==
+
+== Installation ==
+1. Upload the `tweakly` folder to the `/wp-content/plugins/` directory or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+= 1.0.0 =
+* Initial release.
---
+++ tweakly.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin Name: Tweakly
+ * Plugin URI:  https://jeweltheme.com/tweakly
+ * Description: Admin Tweaks Plugin
+ * Version:     1.0.0
+ * Author:      Jewel Theme
+ * Author URI:  https://jeweltheme.com
+ * Text Domain: tweakly
+ * Domain Path: /languages/
+ * License: GPLv3 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package Tweakly
+ */
+
+/**
+ * Tweaks init
+ */
+add_action('init', 'tweakly_init');
+function tweakly_init(){
+    tweakly_hide_frontend_admin_bar();
+    tweakly_remove_wp_version_number();
+    add_action( 'admin_menu', 'tweakly_remove_admin_footer_version' );
+}
+/**
+ * Hide Frontend Admin Bar
+ *
+ * @return void
+ */
+function tweakly_hide_frontend_admin_bar(){
+    add_filter( 'show_admin_bar', '__return_false' );
+}
+
+
+/**
+ * Remove Admin Footer Version Number
+ *
+ * @return void
+ */
+function tweakly_remove_admin_footer_version(){
+    // Remove WordPress Version except Admin
+    if ( ! current_user_can( 'manage_options' ) ) {
+        remove_filter( 'update_footer', 'core_update_footer' );
+    }
+}
+
+
+/**
+ * Remove WordPress Version Number
+ *
+ * @return void
+ */
+function tweakly_remove_wp_version_number(){
+    remove_action( 'wp_head', 'wp_generator' ); // Remove WordPress Generator Version
+    add_filter( 'the_generator', '__return_false' ); // Remove Generator Name From Rss Feeds.
+}
